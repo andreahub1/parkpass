5bdd6b7b06de635f2115b9d1e9cc435eb988334a
andreitamilfallas <andrea.ledesmalopez@cesunbc.edu.mx>
2026-04-09 21:22:52 -0700

    pushh jovanny- boleto pagado

--- boletos/urls.py
+++ boletos/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('buscar-boleto/<uuid:uuid>/', views.buscar_boleto, name='buscar_boleto'),
-     path('procesar-pago/<int:id>/', views.procesar_pago, name='procesar_pago'),
-]
+    path('procesar-pago/<int:id>/', views.procesar_pago, name='procesar_pago'),
+    path('confirmar-pago/', views.confirmar_pago, name='confirmar_pago'),
+    path('pago-paypal/<uuid:boleto_id>/', views.pago_paypal, name='pago_paypal'),]
--- boletos/views.py
+++ boletos/views.py
@@ -2,7 +2,8 @@ from django.http import JsonResponse
 from django.utils import timezone
 from .models import Boleto
 from django.shortcuts import render, get_object_or_404
-
+from django.views.decorators.csrf import csrf_exempt
+import json 
 
 def buscar_boleto(request, uuid):
     try:
@@ -40,3 +41,35 @@ def procesar_pago(request, id):
         "boleto": boleto,
         "metodo": metodo
     })
+
+
+# 🔥 PAYPAL (CORREGIDO)
+def pago_paypal(request, boleto_id):
+    boleto = get_object_or_404(Boleto, codigo=boleto_id)
+
+    # ✅ FORZAR FORMATO CORRECTO PARA PAYPAL
+    monto = "{:.2f}".format(float(boleto.monto))
+
+    return render(request, "usuarios/pago_paypal.html", {
+        "monto": monto,
+        "boleto_id": boleto.codigo
+    })
+    
+# ✅ CONFIRMAR PAGO DESDE PAYPAL
+@csrf_exempt
+def confirmar_pago(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        boleto_id = data.get("boleto_id")
+
+        try:
+            boleto = Boleto.objects.get(codigo=boleto_id)
+            boleto.pagado = True
+            boleto.save()
+
+            return JsonResponse({"status": "ok"})
+
+        except Boleto.DoesNotExist:
+            return JsonResponse({"error": "No existe"})
+    
+    
